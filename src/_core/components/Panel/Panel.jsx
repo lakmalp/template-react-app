dbd95ab339e2dc7dae661f40158151c41c31b5fc
lakmalp <lakmalp@gmail.com>
2022-02-16 12:25:18 +0530

    testing Readme.md for each component

--- src/_core/components/Panel/Panel.jsx
+++ src/_core/components/Panel/Panel.jsx
@@ -1,5 +1,11 @@
-export default function Panel({ className, children}) {
+import PropTypes from 'prop-types';
+
+export default function Panel({ className, children }) {
   return (
     <div className={className}>{children}</div>
   )
 }
+
+Panel.propTypes = {
+  className: PropTypes.string
+};
---
+++ src/_core/components/Readme.md
@@ -0,0 +1,3 @@
+## List of components
+1. ApiWaiting
+2. BreadCrumbs
